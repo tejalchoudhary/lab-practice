c5b4771844c8ba1059f39bc186ee37189ec78367
tejalchoudhary <tejal292000@gmail.com>
2022-01-15 15:30:50 +0530

    Apis added

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -15,5 +15,42 @@ router.get('/',(request,response)=>{
     })
 })
 
-
+router.post('/',(request,response)=>{
+    const{movie_id, movie_title,movie_release_date,movie_time,director_name} = request.body
+    const conn = con.openConnection()
+    conn.query(`INSERT INTO movie(movie_id, movie_title,movie_release_date,movie_time,director_name) 
+    VALUES(${movie_id},'${movie_title}','${movie_release_date}','${movie_time}','${director_name}')`,(error,data)=>{
+        conn.end()
+        if(error){
+            response.send(error)
+        }else{
+            response.send(data)
+        }
+    })
+})
+router.put('/:movie_id',(request,response)=>{
+    const{movie_id} = request.params
+    const{movie_title,movie_time} = request.body
+    const conn = con.openConnection()
+    conn.query(`UPDATE movie SET movie_title='${movie_title}' AND movie_time='${movie_time}' WHERE movie_id = ${movie_id}`,(error,data)=>{
+        conn.end()
+        if(error){
+            response.send(error)
+        }else{
+            response.send(data)
+        }
+    })
+})
+router.delete('/:id',(request,response)=>{
+    const{movie_id} = request.params
+    const conn = con.openConnection()
+    conn.query(`DELETE FROM movie WHERE movie_id=${movie_id};`,(error,data)=>{
+        conn.end()
+        if(error){
+            response.send(error)
+        }else{
+            response.send(data)
+        }
+    })
+})
 module.exports = router
